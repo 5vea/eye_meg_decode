59d9b26bcd0602d35d285509c5a69281e4cd78a5
sebif <sebi.fohs@outlook.de>
2024-12-06 18:53:46 +0100

    simulate and preprocess data, for dataloaders

---
+++ preprocess_eye/processable_format.py
@@ -0,0 +1,37 @@
+"""
+This code will read the simulated data and also bin the data into 50 ms bins.
+
+Flattens in the end, because we want to explore the data without bin systematics.
+
+Assumes fs = 1000 Hz.
+"""
+
+import numpy as np
+
+bin_size = 50 # ms
+flatten = True
+
+# load data
+gt_trials = np.load("../simulate_eye/gt_trials.npy", allow_pickle=True)
+trials = np.load("../simulate_eye/trials.npy")
+
+# bin data
+# make to size (n, n_bins, bin_size)
+n_bins = trials.shape[1] // bin_size
+
+binned_trials = np.zeros((trials.shape[0], n_bins, bin_size))
+
+# repmat gt to n_bins
+gt_trials = np.repeat(gt_trials[:,np.newaxis], n_bins, axis=1)
+
+for i in range(n_bins):
+    binned_trials[:, i, :] = trials[:, i*bin_size:(i+1)*bin_size]
+
+# check if data format should be flattened before output (no bin systematics)
+if flatten:
+    binned_trials = binned_trials.reshape(-1, binned_trials.shape[0])
+    gt_trials = gt_trials.flatten()
+
+# save data
+np.save("../data/data_eye/gt_trials_preprocessed", gt_trials, allow_pickle=True)
+np.save("../data/data_eye/trials_preprocessed", binned_trials)
---
+++ simulate_eye/sim_data.py
@@ -0,0 +1,32 @@
+"""
+Here we will simulate some data to test the functions in the other files.
+This serves to be able to work on the project, until we have settled on a dataset to use.
+
+In the current state it will only return random data, but if we cannot settle on a dataset, we can extend this to
+simulate systematic differences between the classes.
+
+Assumes fs = 1000 Hz.
+"""
+
+import numpy as np
+
+def simulate_trials(n=1000, gts=["1","2","3","4"], ts=10000, v=False):
+    """
+    Simulate n trials, with timepoints ts, and possible ground truths gts.
+    """
+    gt_trials = np.random.choice(gts, n)
+    trials = np.random.randn(n, ts)
+
+    if v:
+        print("Size GT", gt_trials.shape)
+        print("Size Trials", trials.shape)
+
+    return gt_trials, trials
+
+# simulate data, from gabor patches with different orientations
+# 0° = 180°, 45° = 225°, 90° = 270°, 135° = 315° --> this is why we stop at 135°
+gt_trials, trials = simulate_trials(n=1000, gts=["0°", "45°", "90°", "135°"], ts=2000, v=True)
+
+# save data
+np.save("gt_trials.npy", gt_trials, allow_pickle=True)
+np.save("trials.npy", trials)
