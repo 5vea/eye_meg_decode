0fd488ab9986ed5eedd309663f6c54984d4da6bc
5vea <svea-katharina.kuerten@psychol.uni-giessen.de>
2025-03-12 10:51:45 +0100

    adapted training script for contrastive learning

--- decode_eye/train_cl_model.py
+++ decode_eye/train_cl_model.py
@@ -1,44 +1,71 @@
-"""
-This script trains a model on the eye data.
+"""This script trains a model on the eye data.
 
-It is not the aim to find the best possible classifier, which is why we will use a simple CNN and a simple transformer model.
-Furthermore, due to data shortage, we will only use a subset of the labels for training (1/3 random per subject).
-The model will have a test set comprising of one image per label, which will be used to evaluate the model and check for overfitting.
+This script is adapted on the train_model.py script. Just for contrastive learning, so training loop and data loading is adapted.
 
-Also for the loss function, the classes will be weighted, so that the model is not biased towards the majority class.
-We will use a cross entropy loss function with logarithmic weights, to give more weight to the minority classes, but not too much.
+We will also work with 2/3 of the labels, to have a more balanced dataset. And 1/3 left out labels for later evaluation.
+Furthermore one item per training label is left out for epoch evaluation.
 
-Cosine annealing will be used to update the learning rate, which will start at init_lr and end at min_lr. The scheduler will be updated after each epoch.
-Using it to avoid overfitting and to find a good learning rate. We will not use restarts, because those might lead to more overfitting.
-"""
+Dataloaders done
+Model done
+Training done
 
-"""
+Check save in eval
+
 To Do:
-better folder management!
-tensorboard - load split
-epoch start from last epoch
+Evaluation
+Interpretation
+- kernel probing in cnn (mechanistic interpretation)
+- attention maps in transformer (temporal interpretation)
 """
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "GPU-f4bd4d61-6e02-6347-559f-dc9c6528303e"
+os.environ["CUDA_VISIBLE_DEVICES"] = "GPU-951bdf40-e7e4-368e-ca9e-d63cf9d23f30"
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import torch.optim as optim
 import numpy as np
-import transformer_model as tf
-import CNN_model as cnn
+import transformer_cl_model as tf
+import cnn_cl_model as cnn
 import argparse
 import json
 import pandas as pd
 from torch.utils.tensorboard import SummaryWriter
+import torchvision
+from PIL import Image
+import itertools
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def load_checkpoint(model, optimizer, scheduler, path):
+    checkpoint_path = os.path.join(path, 'checkpoint.pth')
+    if os.path.exists(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        scheduler.load_state_dict(checkpoint['scheduler'])
+        start_epoch = checkpoint['epoch'] + 1
+        print(f"Checkpoint loaded from epoch {start_epoch}")
+        return start_epoch
+    return 0
+
+def save_checkpoint(model, optimizer, scheduler, epoch, path):
+    checkpoint = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict(),
+    }
+    torch.save(checkpoint, os.path.join(path, 'checkpoint.pth'))
+    print(f"Checkpoint saved at epoch {epoch}")
+
+
+
 #%% setup argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", type=int, default=15, help="Batch size.")
+parser.add_argument("--batch_size", type=int, default=50, help="Batch size.")
 parser.add_argument("--epochs", type=int, default=1000, help="Number of epochs.")
 parser.add_argument("--init_lr", type=float, default=0.0003, help="Learning rate at start.")
 parser.add_argument("--min_lr", type=float, default=0.00001, help="Learning rate at end of cos annealing.")
@@ -47,10 +74,11 @@ parser.add_argument("--bin", type=int, default=0, help="Only false implemented."
 parser.add_argument("--load", type=int, default=0, help="Load existing model.")
 parser.add_argument("--model_id", type=int, default=0, help="Model id.")
 parser.add_argument("--subj", type=int, default=1, help="Subject to train.")
+parser.add_argument("--vit_lr", type=float, default=0.0001, help="Learning rate for the ViT model.")
 
 args = parser.parse_args()
 
-model_folder = "/home/saskia_fohs/mbb_dl_project/models/subj" + str(args.subj) + "/" + args.model + "_" + str(args.model_id) + "_" + str(args.epochs) + "_" + str(args.init_lr).replace(".", "-") + "_" + str(args.batch_size)
+model_folder = "/home/saskia_fohs/mbb_dl_project/models/subj" + str(args.subj) + "/" + args.model + "_CL" + "_" + str(args.model_id) + "_" + str(args.epochs) + "_" + str(args.init_lr).replace(".", "-") + "_" + str(args.batch_size)
 
 print(args.model)
 
@@ -92,7 +120,7 @@ writer = SummaryWriter(model_folder)
 
 # %% Dataset
 class ReadData(torch.utils.data.Dataset):
-    def __init__(self, gts_path, eye_path, classes):
+    def __init__(self, gts_path, eye_path, classes, img_transforms=None, img_path=None):
         """
         Args:
             gts_path (str): Path to the ground truth labels .npy file.
@@ -100,6 +128,10 @@ class ReadData(torch.utils.data.Dataset):
         """
         self.gts = np.load(gts_path)
         self.eye = np.load(eye_path)
+
+        self.img_transforms = img_transforms
+
+        self.img_path = img_path
 
         # correct dataset for test images
         self.corrector()
@@ -111,8 +143,8 @@ class ReadData(torch.utils.data.Dataset):
         if override:
             self.labels_train = np.load(model_folder + "/labels_train.npy")
         else:
-            # create labels_train from one third of label output
-            self.labels_train = np.random.choice(self.label_output, int(len(self.label_output)/3), replace=False)
+            # create labels_train from two thirds of label output
+            self.labels_train = np.random.choice(self.label_output, int(len(self.label_output)/3) * 2, replace=False)
             np.save(model_folder + "/labels_train.npy", self.labels_train)
 
         # only use chosen labels for training
@@ -138,8 +170,8 @@ class ReadData(torch.utils.data.Dataset):
 
     def train_test_split(self):
         """
-        Returns the indices of the training and test set. The test set is one image per label, chosen randomly, if load is false.
-        If load is true, the indices are loaded from the model folder.
+        Returns the indeces of the training and test set. The test set is one image per label, chosen randomly, if load is false.
+        If load is true, the indeces are loaded from the model folder.
         """
         pass
         if args.load:
@@ -177,6 +209,12 @@ class ReadData(torch.utils.data.Dataset):
     def get_class(self, id):
         return self.classes[self.classes["things_category_nr"] == id]["category"].values[0]
 
+    def get_image(self, id):
+        """
+        Returns the image path for a given image id.
+        """
+        return self.classes[self.classes["things_image_nr"] == id]["image_path"].values[0]
+
     def __len__(self):
         """
         Returns the total number of samples in the dataset.
@@ -191,26 +229,37 @@ class ReadData(torch.utils.data.Dataset):
         # Get data
         eye_data = self.eye[idx]
         label = self.gts[idx,0]
+        img_id = self.gts[idx,1]
+
+        img_spec = self.get_image(img_id)
 
         one_hot = self.label_output == label
 
         # to index
         label = np.argmax(one_hot)
 
-        return eye_data, label
+        if self.img_transforms is not None:
+            img = Image.open(self.img_path + "/" + img_spec)
+            img = self.img_transforms(img)
+        else:
+            img = None
+
+        data = {"eye_data": eye_data.astype(np.float32), "label": label, "img": img}
+
+        return data
 
 # %% Prep the sets for the loader
 # Create dataset
 data_directory = "/home/saskia_fohs/mbb_dl_project/preprocessed"
+img_dir = "/home/saskia_fohs/mbb_dl_project/things_images"
 eye_path = data_directory + "/trial_data_P" + str(args.subj) + ".npy"
 gt_path = data_directory + "/trial_gt_P" + str(args.subj) + ".npy"
 classes = pd.read_csv(data_directory + "/categories.csv")
 
-dataset = ReadData(gts_path=gt_path, eye_path=eye_path, classes=classes)
+# vit image transform
+transform = torchvision.models.ViT_B_32_Weights.IMAGENET1K_V1.transforms()
 
-# get class weights to balance the loss function -- not super clean, because call before train_test_split, but subset
-# does not inherit the class weights
-class_weights_training = dataset.class_weights()
+dataset = ReadData(gts_path=gt_path, eye_path=eye_path, classes=classes, img_path=img_dir, img_transforms=transform)
 
 # split the dataset into training and test set
 train_idx, test_idx = dataset.train_test_split()
@@ -228,153 +277,87 @@ dataloaders = {"train": train_dl, "val": test_dl}
 
 
 # %% Training -- might be modularized later
-def accuracy(output, target, topk=(1,)):
-    """
-    Computes the accuracy over the k top predictions for the specified values of k.
-    Args:
-        output: model output
-        target: target labels
-        topk: tuple of top k accuracies to calculate
-    Returns:
-        res: list of top k accuracies
-    """
-
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-
-# First try
-def epoch_loop(model_epoch, data, criterion, optimizer=None, scheduler=None):
+def epoch_loop(model_epoch, data, optimizer=None, scheduler=None):
     is_train = False if optimizer is None else True
 
     # set train/eval accordingly
     model_epoch.train() if is_train else model_epoch.eval()
+
+    lr = None
+
+    losses = []
     with torch.set_grad_enabled(is_train):
-        running_loss = torch.zeros(1).to(device)
-        running_accs = torch.zeros(3).to(device)
-        count_batches = 0
-        for input, target in data:
-            # convert to torch float
-            input = input.float()
-
-            input, target = input.to(device), target.to(device)
-
-            outputs = model_epoch(input) # forward pass
-
-            loss = criterion(outputs, target) # loss function
-            acc = accuracy(outputs, target, topk=(1,5,100))
+        for batch in data:
+            batch = {k: v.to(device) for k, v in batch.items() if k != "label"}
+            loss = model_epoch(batch)
+            losses.extend([loss.item() for _ in range(batch["img"].size(0))])
 
             if is_train:
                 optimizer.zero_grad() # reset gradients
                 loss.backward() # backpropagation of loss
                 optimizer.step() # update weights
-            else:
-                running_loss += loss
-                running_accs += torch.tensor(acc).to(device)
-
-            count_batches += 1
 
         if is_train:
             scheduler.step() # update learning rate
+            lr = scheduler.get_last_lr()[0]
 
-    # return loss and accuracy, depending on train or eval
-    if is_train:
-        return loss, acc[0], acc[1], acc[2]
-    else:
-        running_loss /= torch.tensor(count_batches).to(device)
-        running_accs /= torch.tensor(count_batches).to(device)
-        return running_loss, running_accs[0], running_accs[1], running_accs[2]
+    # return losses over batches
+    return losses, lr
 
-def train_model(model, dl_dict, criterion, optimizer, scheduler, num_epochs=100):
+def train_model(model, dl_dict, optimizer, scheduler, num_epochs=100, epoch_start=0):
     model.to(device)
 
+    modes = ["train", "val"]
+
     # update model weights by looping through dataset multiple times
-    for epoch in range(num_epochs):
-        epoch_loss, epoch_acc, epoch_acc_5, epoch_acc_100 = epoch_loop(model, dl_dict["train"], criterion, optimizer, scheduler)
-        print(f"Train: Epoch {epoch+1}/{num_epochs},\t "
-              f"Loss: {float(epoch_loss.cpu().detach().numpy()):.4f},\t "
-              f"Acc: {float(epoch_acc.cpu().detach().numpy()):.2f}%,\t "
-              f"Acc@5: {float(epoch_acc_5.cpu().detach().numpy()):.2f}%,\t "
-              f"Acc@100: {float(epoch_acc_100.cpu().detach().numpy()):.2f}%")
-        writer.add_scalar("Loss/train", float(epoch_loss.cpu().detach().numpy()), epoch)
-        writer.add_scalar("Acc/train", float(epoch_acc.cpu().detach().numpy()), epoch)
-        writer.add_scalar("Acc@5/train", float(epoch_acc_5.cpu().detach().numpy()), epoch)
-        writer.add_scalar("Acc@100/train", float(epoch_acc_100.cpu().detach().numpy()), epoch)
-        epoch_loss, epoch_acc, epoch_acc_5, epoch_acc_100 = epoch_loop(model, dl_dict["val"], criterion, None, scheduler)
-        print(f"Test: Epoch {epoch + 1}/{num_epochs},\t "
-              f"Loss: {float(epoch_loss.cpu().detach().numpy()):.4f},\t "
-              f"Acc: {float(epoch_acc.cpu().detach().numpy()):.2f}%,\t "
-              f"Acc@5: {float(epoch_acc_5.cpu().detach().numpy()):.2f}%,\t "
-              f"Acc@100: {float(epoch_acc_100.cpu().detach().numpy()):.2f}% (all running)")
-        writer.add_scalar("Loss/test", float(epoch_loss.cpu().detach().numpy()), epoch)
-        writer.add_scalar("Acc/test", float(epoch_acc.cpu().detach().numpy()), epoch)
-        writer.add_scalar("Acc@5/test", float(epoch_acc_5.cpu().detach().numpy()), epoch)
-        writer.add_scalar("Acc@100/test", float(epoch_acc_100.cpu().detach().numpy()), epoch)
+    for epoch in range(epoch_start, num_epochs + epoch_start):
+        for mode in modes:
+            losses, lr = epoch_loop(model, dl_dict[mode], optimizer, scheduler)
+            loss = np.mean(losses)
+            print(f"{mode.capitalize()}: Epoch {epoch+1}/{num_epochs},\t "
+                  f"Loss: {loss:.4f}")
+            writer.add_scalar(f"Loss/{mode}", loss, epoch)
+            if lr is not None:
+                writer.add_scalar("Learning Rate", lr, epoch)
 
         # save checkpoint after each epoch
-        save_checkpoint(model, optimizer, scheduler, epoch, path)
+        save_checkpoint(model, optimizer, scheduler, epoch, model_folder)
 
 # create model
 if args.model == "cnn":
-    model = cnn.ResNet1D(in_channels=3, num_classes=len(dataset.label_output), patch_size=3)
+    model = cnn.ContrastiveModel_cnn()
 elif args.model == "transformer":
-    model = tf.EyeTransformerEncClass(num_layers=1, num_heads=4, n_classes=len(dataset.label_output), embedding_dim=32, dropout_rate=0.3)
+    model = tf.ContrastiveModel_tf()
 else:
     raise ValueError("Model not found.")
 
 # parameter count
 print(f"Model has {sum(p.numel() for p in model.parameters() if p.requires_grad)} parameters.")
 
-criterion = nn.CrossEntropyLoss(weight=class_weights_training).to(device)
-optimizer = optim.AdamW(model.parameters(), lr=args.init_lr, weight_decay=0.01)
+# different lr for different parts of the model
+params = [
+        #{"params": model.image_encoder.parameters(), "lr": args.vit_lr, "weight_decay": 0.},
+        {"params": model.eye_encoder.parameters(), "lr": args.init_lr},
+        {"params": itertools.chain(
+            model.projection_eye.parameters(), model.projection_image.parameters()
+        ), "lr": args.init_lr}
+    ]
 
-# cosin annealing, no restarts
+# optimizer
+optimizer = optim.AdamW(params, weight_decay=0.01)
+
+# cosine annealing, no restarts
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100, eta_min=args.min_lr)
 
 # to device
 model.to(device)
 
+if args.load:
+    start_epoch = load_checkpoint(model, optimizer, scheduler, model_folder)
+else:
+    start_epoch = 0
+
 # train model
-train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs=args.epochs)
+train_model(model, dataloaders, optimizer, scheduler, num_epochs=args.epochs, epoch_start = start_epoch)
 
 writer.close()
-
-
-# save model, optimizer, scheduler
-path = model_folder
-
-def save_checkpoint(model, optimizer, scheduler, epoch, path):
-    checkpoint = {
-        'epoch': epoch,
-        'state_dict': model.state_dict(),
-        'optimizer': optimizer.state_dict(),
-        'scheduler': scheduler.state_dict(),
-    }
-    torch.save(checkpoint, os.path.join(path, 'checkpoint.pth'))
-    print(f"Checkpoint saved at epoch {epoch}")
-
-def load_checkpoint(model, optimizer, scheduler, path):
-    checkpoint_path = os.path.join(path, 'checkpoint.pth')
-    if os.path.exists(checkpoint_path):
-        checkpoint = torch.load(checkpoint_path)
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        scheduler.load_state_dict(checkpoint['scheduler'])
-        start_epoch = checkpoint['epoch'] + 1
-        print(f"Checkpoint loaded from epoch {start_epoch}")
-        return start_epoch
-    else:
-        print("No checkpoint found, starting from scratch.")
-        return 0
-
-
